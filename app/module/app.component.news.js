74ae450b1ba7a56863b0a479dd545fdbcd4b7c39
devarchi33 <devarchi33@gmail.com>
2016-07-14 11:56:54 +0900

    1. news contents  ngFor 처리. 2. 미디어 쿼리 적용.

--- app/module/app.component.news.js
+++ app/module/app.component.news.js
@@ -5,22 +5,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 /**
  * Created by donghoon on 2016. 7. 11..
  */
 var core_1 = require('@angular/core');
 var NewsComponent = (function () {
     function NewsComponent() {
+        this.introduces = [
+            {
+                "content": "中韩跨境环境下的IT咨询、系统设计、开发"
+            },
+            {
+                "content": "为中韩联合创业留学生们提供孵化环境、天使投资"
+            }
+        ];
+        this.newses = [
+            {
+                "date": "06/20",
+                "content": "Tony Ahn delivers a lecture on iOS programming at Fast Track Campus until the end of month."
+            },
+            {
+                "date": "06/14",
+                "content": "bettercode signed an MOU with China Weihai Beiyang Incubation Center."
+            }
+        ];
     }
     NewsComponent = __decorate([
         core_1.Component({
             selector: "news",
             templateUrl: "app/module/news.html"
-        }), 
-        __metadata('design:paramtypes', [])
+        })
     ], NewsComponent);
     return NewsComponent;
 }());
--- app/module/app.component.news.ts
+++ app/module/app.component.news.ts
@@ -8,4 +8,24 @@ import {Component} from '@angular/core';
     templateUrl: "app/module/news.html"
 })
 export class NewsComponent {
+
+    public introduces = [
+        {
+            "content": "中韩跨境环境下的IT咨询、系统设计、开发",
+        },
+        {
+            "content": "为中韩联合创业留学生们提供孵化环境、天使投资"
+        }
+    ];
+
+    public newses = [
+        {
+            "date": "06/20",
+            "content": "Tony Ahn delivers a lecture on iOS programming at Fast Track Campus until the end of month."
+        },
+        {
+            "date": "06/14",
+            "content": "bettercode signed an MOU with China Weihai Beiyang Incubation Center."
+        }
+    ];
 }
