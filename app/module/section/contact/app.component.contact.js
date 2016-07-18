de8c19641e725b00b03a361d88e4eb5c2a27aded
devarchi33 <devarchi33@gmail.com>
2016-07-19 01:28:41 +0900

    지도수정.

--- app/module/section/contact/app.component.contact.js
+++ app/module/section/contact/app.component.contact.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 /**
  * Created by donghoon on 2016. 7. 11..
  */
@@ -21,7 +18,7 @@ var ContactComponent = (function () {
         // google maps zoom level
         this.zoom = 4;
         // initial center position for the map
-        this.lat = 33.77261;
+        this.lat = 34.27261;
         this.lng = 122.036225;
         this.markers = [
             {
@@ -83,8 +80,7 @@ var ContactComponent = (function () {
             directives: [core_2.GOOGLE_MAPS_DIRECTIVES],
             styleUrls: ["app/module/section/contact/contact.css"],
             templateUrl: "app/module/section/contact/contact.html"
-        }), 
-        __metadata('design:paramtypes', [])
+        })
     ], ContactComponent);
     return ContactComponent;
 }());
