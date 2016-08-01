958eb2da2115923383258281b82d0bb4aa575226
devarchi33 <devarchi33@gmail.com>
2016-08-02 07:31:34 +0900

    1. angular2 google map 모듈에 api key 적용.

--- app/main.js
+++ app/main.js
@@ -8,6 +8,13 @@ var app_component_1 = require('./app.component');
 var core_2 = require('angular2-google-maps/core');
 core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
-    core_2.GOOGLE_MAPS_PROVIDERS
+    core_2.GOOGLE_MAPS_PROVIDERS,
+    core_1.provide(core_2.LazyMapsAPILoaderConfig, {
+        useFactory: function () {
+            var config = new core_2.LazyMapsAPILoaderConfig();
+            config.apiKey = 'AIzaSyBt9OsYuGxAYepF-GKbdgcBF32M_W3d5wY';
+            return config;
+        }
+    })
 ]);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,20 @@
 /**
  * Created by donghoon on 2016. 7. 11..
  */
-import {enableProdMode} from '@angular/core';
+import {enableProdMode, provide} from '@angular/core';
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
-import {GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
+import {GOOGLE_MAPS_PROVIDERS, LazyMapsAPILoaderConfig} from 'angular2-google-maps/core';
 
 enableProdMode();
 
 bootstrap(AppComponent, [
-    GOOGLE_MAPS_PROVIDERS
+    GOOGLE_MAPS_PROVIDERS,
+    provide(LazyMapsAPILoaderConfig, {
+        useFactory: () => {
+            let config = new LazyMapsAPILoaderConfig();
+            config.apiKey = 'AIzaSyBt9OsYuGxAYepF-GKbdgcBF32M_W3d5wY';
+            return config;
+        }
+    })
 ]);
